a185ccc77c0b9870d1b7943955942a7f17d5185a
Md Zafir Hasan <mr.zafirhasan@gmail.com>
2025-07-04 23:11:22 +0530

    Day 5

---
+++ ARRAYS/SortAnArrayOf012.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int n0=0,n1=0,n2=0,n=nums.length;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==0)
+            n0++;
+            else if(nums[i]==1)
+            n1++;
+            else
+            n2++;
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(i<n0)
+            nums[i]=0;
+            else if(i>=n0&&i<n1+n0)
+            nums[i]=1;
+            else
+            nums[i]=2;
+        }
+    }
+}
